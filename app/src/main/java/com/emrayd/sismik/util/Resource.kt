5793f661b6df9d98b129a4dfd3c79ec83f46f8fb
Emre Aydoğdu <144744820+EmrAYD@users.noreply.github.com>
2026-06-19 18:01:46 +0300

    Update Resource.kt

--- app/src/main/java/com/emrayd/sismik/util/Resource.kt
+++ app/src/main/java/com/emrayd/sismik/util/Resource.kt
@@ -7,8 +7,8 @@ package com.emrayd.sismik.util
  *  - Success: veri başarıyla geldi
  *  - Error: bir hata oluştu, mesaj ve (varsa) eski/cache'lenmiş veri taşınabilir
  */
-sealed class Resource<T> {
-    data class Success<T>(val data: T) : Resource<T>()
-    data class Error<T>(val message: String) : Resource<T>()
-    class Loading<T> : Resource<T>()
+sealed class Resource<out T> {
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Error<out T>(val message: String, val data: T? = null) : Resource<T>()
+    data class Loading<out T>(val data: T? = null) : Resource<T>()
 }
