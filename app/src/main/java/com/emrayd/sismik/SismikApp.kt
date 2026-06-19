c17c7dfa84c0a49052537e4a538e1620921828a0
Emre Aydoğdu <144744820+EmrAYD@users.noreply.github.com>
2026-06-19 15:37:25 +0300

    DI yazım devam

--- app/src/main/java/com/emrayd/sismik/SismikApp.kt
+++ app/src/main/java/com/emrayd/sismik/SismikApp.kt
@@ -1,7 +1,19 @@
 package com.emrayd.sismik
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SismikApp : Application()
+class SismikApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
