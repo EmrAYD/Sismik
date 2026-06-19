fffede6f3e58a84e71555569df7a8b113a8db568
Emre Aydoğdu <144744820+EmrAYD@users.noreply.github.com>
2026-06-19 12:24:23 +0300

    API istekleri yazıldı

--- app/src/main/java/com/emrayd/sismik/data/remote/api/EarthquakeApiService.kt
+++ app/src/main/java/com/emrayd/sismik/data/remote/api/EarthquakeApiService.kt
@@ -1,4 +1,22 @@
 package com.emrayd.sismik.data.remote.api
 
-class EarthquakeApiService {
+import com.emrayd.sismik.data.remote.dto.EarthquakeResponseDto
+import com.emrayd.sismik.util.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface EarthquakeApiService {
+
+    @GET("deprem/kandilli/live")
+    suspend fun getLiveEarthquakes(
+        @Query("skip") skip: Int = 0,
+        @Query("limit") limit: Int = Constants.DEFAULT_FETCH_LIMIT
+    ): EarthquakeResponseDto
+
+    @GET("deprem/kandilli/archive")
+    suspend fun getArchiveEarthquakes(
+        @Query("start") startDate: String,
+        @Query("end") endDate: String,
+        @Query("limit") limit: Int = Constants.DEFAULT_FETCH_LIMIT
+    ): EarthquakeResponseDto
 }
